a2202c4915bbd1a6530f0d06d83d6d1b8c68a72e
Marcus Vinicius Magdaleno Vieira <marcusvmvieira@gmail.com>
2024-08-05 00:29:36 +0000

    Criação de mapeamento para entrey point da aplicação

---
+++ index.js
@@ -0,0 +1,20 @@
+import {express} from "express";
+import {pkg} from "body-parser";
+import {router} from "./routes/router.js";
+
+// Declaração de constutor de uma objeto express
+const app = express();
+//Uso para requisição e resposta
+const {json,urlencoded} = pkg;
+//Para a aplicação usar o pacote json
+app.use(json());
+//Para uso no mapeamento precisamos incluir o dicionário de parâmetros
+app.use(urlencoded({extended:true}));
+
+//Declaração de escuta de porta 3000 para carregamento do servidor
+app.listen(3000, function(){
+    console.log("Listening port 3000!")
+})
+
+//Declaração rota raíz vindo do módulo router
+app.use("/", router)
