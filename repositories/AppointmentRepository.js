f326e46dbc6e358f994edcd6646dc5af3ea84636
Marcus Vinicius Magdaleno Vieira <marcusvmvieira@gmail.com>
2024-08-11 15:41:16 +0000

    Implementação de métodos CRUD para uso da entidade Appointment

--- repositories/AppointmentRepository.js
+++ repositories/AppointmentRepository.js
@@ -1,5 +1,50 @@
-const appointmentRepository = {
+import {Appointment} from "../models/Appointment.js"
 
+//Declaração de métodos assíncronos para leitura (READ) de dados da entidade Appointment
+//Get All Registers
+const getAllAppointments = async() => {
+    return await Appointment.find();
+}
+//Get a Register by Id
+const getAppointment = async(id) => {
+    try {
+        return await Appointment.findById(id);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+//Declaração de método assíncrono para gravação (CREATE) de um Appointment criando umm novo Prescription por relacionamento
+const saveAppointment = async({date, doctorId, pacientId}) => {
+    try {
+        const prescription = new Appointment({date, doctorId, pacientId});
+        return await prescription.save();
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+//Declaração de método assíncrono para atualização (UPDATE) de um Appointment, e senão houver registro, será criado automaticamente
+const updateAppointment = async(id, {date, doctorId, pacientId}) => {
+    try {
+        return await Appointment.findByIdandUpdate(id, {date, doctorId, pacientId}, {new: true})
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+//
+const deleteAppointment = async(id) => {
+    try {
+        return await Appointment.findByIdandUpdate(id);
+    } catch (error) {
+        throw new Error(erro)
+    }
+}
+
+const appointmentRepository = {
+    getAllAppointments,
+    getAppointment,
+    saveAppointment,
+    updateAppointment,
+    deleteAppointment
 };
 
 export default appointmentRepository;
